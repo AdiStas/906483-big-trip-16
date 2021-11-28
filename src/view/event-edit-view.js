f76fcc4c315afaea39b96dc85ff93643652da6bd
adiStas <adistas91@yandex.ru>
2021-11-28 21:38:03 +0300

    переименовал функции создания компонент

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -1,4 +1,4 @@
-export const createPointEditTemplate = () => (
+export const createEventEditTemplate = () => (
   `<li class="trip-events__item">
               <form class="event event--edit" action="#" method="post">
                 <header class="event__header">
