d93f885336ba3fabd9a56d09a6d985bad92139a9
adiStas <adistas91@yandex.ru>
2021-12-19 23:36:26 +0300

    Добавил файл с константами

---
+++ src/const.js
@@ -0,0 +1,82 @@
+export const EVENT_COUNT = 15;
+export const HOUR = 60;
+export const DAY = 24;
+export const TYPES = [
+  {
+    title: 'Taxi',
+    checked: '',
+  },
+  {
+    title: 'Bus',
+    checked: '',
+  },
+  {
+    title: 'Train',
+    checked: '',
+  },
+  {
+    title: 'Ship',
+    checked: '',
+  },
+  {
+    title: 'Drive',
+    checked: '',
+  },
+  {
+    title: 'Flight',
+    checked: '',
+  },
+  {
+    title: 'Check-in',
+    checked: '',
+  },
+  {
+    title: 'Sightseeing',
+    checked: '',
+  },
+  {
+    title: 'Restaurant',
+    checked: '',
+  },
+];
+export const DESTINATIONS = ['Amsterdam', 'Geneva', 'Chamonix', 'Berlin', 'Madrid'];
+export const DESCRIPTIONS = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.',
+];
+export const OFFERS = [
+  {
+    name: 'luggage',
+    title: 'Add luggage',
+    price: '50',
+  },
+  {
+    name: 'comfort',
+    title: 'Switch to comfort',
+    price: '80',
+  },
+  {
+    name: 'meal',
+    title: 'Add meal',
+    price: '15',
+  },
+  {
+    name: 'seats',
+    title: 'Choose seats',
+    price: '5',
+  },
+  {
+    name: 'train',
+    title: 'Travel by train',
+    price: '40',
+  },
+];
