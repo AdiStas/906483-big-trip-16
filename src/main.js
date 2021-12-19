f7506f5d1edb520580af985032757533852da9a8
adiStas <adistas91@yandex.ru>
2021-12-19 23:35:36 +0300

    добавил работу компонент с внешними данными

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import {EVENT_COUNT} from './const';
 import {renderTemplate} from './render';
 import {createSiteMenuTemplate} from './view/site-menu-view';
 import {createFilterTemplate} from './view/filter-view';
@@ -5,8 +6,9 @@ import {createSortTemplate} from './view/sorting-view';
 import {createEventsListTemplate} from './view/events-list-view';
 import {createEventTemplate} from './view/event-view';
 import {createEventEditTemplate} from './view/event-edit-view';
+import {generateEventPoint} from './mock/event-point';
 
-const EVENT_COUNT = 3;
+const eventPoint = Array.from({length: EVENT_COUNT}, generateEventPoint);
 
 const siteHeaderElement = document.querySelector('.page-header');
 const siteNavElement = siteHeaderElement.querySelector('.trip-controls__navigation');
@@ -23,8 +25,8 @@ renderTemplate(pageMainContentElement, createEventsListTemplate());
 
 const eventsListElement = pageMainContentElement.querySelector('.trip-events__list');
 
-renderTemplate(eventsListElement, createEventEditTemplate());
+renderTemplate(eventsListElement, createEventEditTemplate(eventPoint[0]));
 
-for (let i = 0; i < EVENT_COUNT; i++) {
-  renderTemplate(eventsListElement, createEventTemplate());
+for (let i = 1; i < EVENT_COUNT; i++) {
+  renderTemplate(eventsListElement, createEventTemplate(eventPoint[i]));
 }
--- src/view/event-add-view.js
+++ src/view/event-add-view.js
@@ -1,104 +1,25 @@
-const eventTypes = [
-  {
-    name: 'taxi',
-    title: 'Taxi',
-    checked: '',
-  },
-  {
-    name: 'bus',
-    title: 'Bus',
-    checked: '',
-  },
-  {
-    name: 'train',
-    title: 'Train',
-    checked: '',
-  },
-  {
-    name: 'ship',
-    title: 'Ship',
-    checked: '',
-  },
-  {
-    name: 'drive',
-    title: 'Drive',
-    checked: '',
-  },
-  {
-    name: 'flight',
-    title: 'Flight',
-    checked: 'checked',
-  },
-  {
-    name: 'check-in',
-    title: 'Check-in',
-    checked: '',
-  },
-  {
-    name: 'sightseeing',
-    title: 'Sightseeing',
-    checked: '',
-  },
-  {
-    name: 'restaurant',
-    title: 'Restaurant',
-    checked: '',
-  },
-];
-const eventOfferSelectors = [
-  {
-    name: 'luggage',
-    title: 'Add luggage',
-    checked: 'checked',
-    price: '50',
-  },
-  {
-    name: 'comfort',
-    title: 'Switch to comfort',
-    checked: 'checked',
-    price: '80',
-  },
-  {
-    name: 'meal',
-    title: 'Add meal',
-    checked: '',
-    price: '15',
-  },
-  {
-    name: 'seats',
-    title: 'Choose seats',
-    checked: '',
-    price: '5',
-  },
-  {
-    name: 'train',
-    title: 'Travel by train',
-    checked: '',
-    price: '40',
-  },
-];
-const eventPhotos = ['1', '2', '3', '4', '5'];
+import {TYPES, DESTINATIONS} from '../const.js';
+import {getDateByFormat} from '../utils';
 
-const createEventItems = eventTypes
-  .map((item) => (
-    `<div class="event__type-item">
+const createEventTypesListTemplate = (type) => TYPES.map((item) => {
+  item.checked = item.title.toLowerCase() === type.title.toLowerCase() ? 'checked' : '';
+  return (`<div class="event__type-item">
       <input
-        id="event-type-${item.name}-1"
+        id="event-type-${item.title.toLowerCase()}-1"
         class="event__type-input  visually-hidden"
         type="radio"
         name="event-type"
-        value="${item.name}"
+        value="${item.title.toLowerCase()}"
         ${item.checked}>
       <label
-        class="event__type-label  event__type-label--${item.name}"
-        for="event-type-${item.name}-1">
+        class="event__type-label  event__type-label--${item.title.toLowerCase()}"
+        for="event-type-${item.title.toLowerCase()}-1">
         ${item.title}
       </label>
-    </div>`))
-  .join('');
-
-const createEventOfferSelectors = eventOfferSelectors
-  .map((item) => (
+    </div>`);
+}).join('');
+const createEventOffersListTemplate = (offers) => {
+  const availableOffers = offers.map((item) => (
     `<div class="event__offer-selector">
       <input
         class="event__offer-checkbox  visually-hidden"
@@ -120,50 +41,85 @@ const createEventOfferSelectors = eventOfferSelectors
         </span>
       </label>
     </div>`))
-  .join('');
+    .join('');
 
-const createEventPhotos = eventPhotos
-  .map((item ) => (
-    `<img class="event__photo" src="img/photos/${item}.jpg" alt="Event photo">`
-  ))
-  .join('');
-export const createEventAddTemplate = () => (
-  `<li class="trip-events__item">
+  if (offers.length > 0) {
+    return `<section class="event__section  event__section--offers">
+              <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+              <div class="event__available-offers">
+                ${availableOffers}
+              </div>
+            </section>`;
+  } else {
+    return '';
+  }
+};
+const createEventDestinationsListTemplate = () => DESTINATIONS.map((item) => `<option value="${item}"></option>`).join('');
+const createEventPicturesTemplate = (destination) => {
+  const pictures = destination.pictures.map((item) => `<img class="event__photo" src="${item.src}" alt="Event photo">`).join('');
+  if (destination.pictures.length > 0) {
+    return `<div class="event__photos-container">
+              <div class="event__photos-tape">
+                ${pictures}
+              </div>
+            </div>`;
+  } else {
+    return '';
+  }
+};
+export const createEventAddTemplate = (eventPoint = {}) => {
+  const {
+    price = '',
+    dateFrom = '',
+    dateTo = '',
+    destination = {
+      description: '',
+      name: '',
+      pictures: [],
+    },
+    offers = [],
+    type = {},
+  } = eventPoint;
+
+  const eventTypeTemplate = createEventTypesListTemplate(type);
+  const eventOfferTemplate = createEventOffersListTemplate(offers);
+  const eventDestinationsTemplate = createEventDestinationsListTemplate();
+  const eventPictureTemplate = createEventPicturesTemplate(destination);
+
+  return `<li class="trip-events__item">
               <form class="event event--edit" action="#" method="post">
                 <header class="event__header">
                   <div class="event__type-wrapper">
                     <label class="event__type  event__type-btn" for="event-type-toggle-1">
                       <span class="visually-hidden">Choose event type</span>
-                      <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+                      <img class="event__type-icon" width="17" height="17" src="img/icons/${type.title.toLowerCase()}.png" alt="Event type icon">
                     </label>
                     <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
                     <div class="event__type-list">
                       <fieldset class="event__type-group">
                         <legend class="visually-hidden">Event type</legend>
-                          ${createEventItems}
+                          ${eventTypeTemplate}
                       </fieldset>
                     </div>
                   </div>
 
                   <div class="event__field-group  event__field-group--destination">
                     <label class="event__label  event__type-output" for="event-destination-1">
-                      Flight
+                      ${type.title.toLowerCase()}
                     </label>
-                    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
+                    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1">
                     <datalist id="destination-list-1">
-                      <option value="Amsterdam"></option>
-                      <option value="Geneva"></option>
-                      <option value="Chamonix"></option>
+                      ${eventDestinationsTemplate}
                     </datalist>
                   </div>
 
                   <div class="event__field-group  event__field-group--time">
                     <label class="visually-hidden" for="event-start-time-1">From</label>
-                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
+                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${getDateByFormat(dateFrom, 'YY/MM/DD HH:mm')}">
                     &mdash;
                     <label class="visually-hidden" for="event-end-time-1">To</label>
-                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/19 00:00">
+                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${getDateByFormat(dateTo, 'YY/MM/DD HH:mm')}">
                   </div>
 
                   <div class="event__field-group  event__field-group--price">
@@ -171,32 +127,23 @@ export const createEventAddTemplate = () => (
                       <span class="visually-hidden">Price</span>
                       &euro;
                     </label>
-                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
                   </div>
 
                   <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-                  <button class="event__reset-btn" type="reset">Cancel</button>
+                  <button class="event__reset-btn" type="reset">Delete</button>
+                  <button class="event__rollup-btn" type="button">
+                    <span class="visually-hidden">Open event</span>
+                  </button>
                 </header>
                 <section class="event__details">
-                  <section class="event__section  event__section--offers">
-                    <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-                    <div class="event__available-offers">
-                      ${createEventOfferSelectors}
-                    </div>
-                  </section>
-
+                  ${eventOfferTemplate}
                   <section class="event__section  event__section--destination">
                     <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-                    <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
-
-                    <div class="event__photos-container">
-                      <div class="event__photos-tape">
-                        ${createEventPhotos}
-                      </div>
-                    </div>
+                    <p class="event__destination-description">${destination.description}</p>
+                    ${eventPictureTemplate}
                   </section>
                 </section>
               </form>
-            </li>`
-);
+  </li>`;
+};
